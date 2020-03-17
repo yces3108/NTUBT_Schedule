164dee3739eaf0e7e230f4b3c3ddd47b6610776d
Po-An Chen <r06521541@ntu.edu.tw>
2020-03-17 17:19:10 +0800

    完善了練球安排功能

---
+++ src/components/ScheduleButtons/PlayerCount.vue
@@ -0,0 +1,146 @@
+<template>
+  <div>
+    <div
+      class="player-count-section"
+      v-for="(position, player) of playerProfile"
+      :key="player"
+      :player="player"
+      :position="position"
+      :strangeCount="strangeCount"
+    >
+      <!-- strangeCount是要讓vue實例知道要重新渲染 -->
+      <div class="player-schedule-button" @click="highlight(player)">
+        <div
+          :class="{
+            highlighted: player == highlightedPlayer,
+            unhighlighted: player != highlightedPlayer,
+            pitcher: isPitcher(player) && player != highlightedPlayer,
+            catcher: isCatcher(player) && player != highlightedPlayer,
+            infielder: isInfielder(player) && player != highlightedPlayer,
+            outfielder: isOutfielder(player) && player != highlightedPlayer
+
+
+
+          }"
+        >
+          {{ player }}
+        </div>
+      </div>
+      <div
+        v-for="(sectionDrill, indexDrill) of playerDrillSections[player]"
+        :key="indexDrill"
+        :strangeCount="strangeCount"
+        class="drill-count"
+      />
+      <div
+        v-for="(sectionWeight, indexWeight) of playerWeightSections[player]"
+        :key="indexWeight"
+        :strangeCount="strangeCount"
+        class="weight-count"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PlayerCount",
+  props: {
+    playerProfile: Object,
+    playerDrillSections: Object,
+    playerWeightSections: Object,
+    highlightedPlayer: String,
+    highlightedPosition: Number,
+    strangeCount: Number
+  },
+  methods: {
+    highlight(player) {
+      this.$emit("highlightPlayer", player);
+    },
+    isPitcher(player) {
+      return this.highlightedPosition == 1 && this.playerProfile[player]['position'].includes("pitcher");
+    },
+    isCatcher(player) {
+      return this.highlightedPosition == 2 && this.playerProfile[player]['position'].includes("catcher");
+    },
+    isInfielder(player) {
+      return this.highlightedPosition == 3 && this.playerProfile[player]['position'].includes("infielder");
+    },
+    isOutfielder(player) {
+      return this.highlightedPosition == 4 && this.playerProfile[player]['position'].includes("outfielder");
+    }
+  },
+  updated() {
+    console.log(this.highlightedPosition);
+  }
+};
+</script>
+
+<style>
+
+.player-schedule-button .highlighted {
+  background-color: rgb(255, 197, 37);
+  color: white;
+  border: 0.5px solid rgb(255, 197, 37);
+}
+.player-schedule-button .unhighlighted {
+  border: 0.5px solid rgb(39, 60, 86);
+}
+.player-schedule-button .pitcher {
+  border: 1px solid rgb(255, 75, 144);
+  background-color: rgb(255, 75, 144);
+  color: white;
+}
+.player-schedule-button .catcher {
+  border: 1px solid rgb(0, 99, 228);
+  background-color: rgb(0, 99, 228);
+  color: white;
+}
+.player-schedule-button .infielder {
+  border: 1px solid rgb(255, 239, 93);
+  background-color: rgb(255, 239, 93);
+  color: rgb(39, 60, 86);
+}
+.player-schedule-button .outfielder {
+  border: 1px solid rgb(51, 156, 2);
+  background-color: rgb(51, 156, 2);
+  color: white;
+}
+.player-count-section {
+  display: inline-block;
+}
+.player-schedule-button {
+  cursor: pointer;
+  display: block;
+  height: 20px;
+  line-height: 20px;
+  width: 20px;
+  margin: 2px 2px;
+  text-align: center;
+  vertical-align: middle;
+}
+.drill-count {
+  display: block;
+  box-sizing: border-box;
+  height: 5px;
+  line-height: 20px;
+  width: 20px;
+  margin: 2px 2px;
+  border: 0.5px solid rgb(39, 60, 86);
+  background-color: rgb(39, 60, 86);
+  text-align: center;
+  vertical-align: middle;
+}
+.weight-count {
+  display: block;
+  box-sizing: border-box;
+  height: 5px;
+  line-height: 20px;
+  width: 20px;
+  margin: 2px 2px;
+  border: 0.5px solid rgb(206, 49, 49);
+  background-color: rgb(206, 49, 49);
+  text-align: center;
+  vertical-align: middle;
+}
+</style>
---
+++ src/components/ScheduleButtons/SchedulePlayerSectionButton.vue
@@ -0,0 +1,158 @@
+<template>
+  <div
+    class="player-section-button cursor-pointer"
+    @click="toggle"
+  >
+    <p 
+      :class="{
+        selectable: isSelectable,
+        drill: isInselectedDrills,
+        weight: isInselectedWeights, 
+        unselectable: (!isInselectedDrills && !isInselectedWeights) && !isSelectable,
+        highlighted: isHighlighted,
+        pitcher: isPitcher && !isHighlighted,
+        catcher: isCatcher && !isHighlighted,
+        infielder: isInfielder && !isHighlighted,
+        outfielder: isOutfielder && !isHighlighted,
+      }">
+      {{ player }}
+    </p>
+  </div>
+</template>
+
+<script>
+//import axios from "axios";
+export default {
+  name: "SchedulePlayerSectionButton",
+  props: {
+    player: String,
+    positions: Array,
+    day: Number,
+    section: Number,
+    category: Number,
+    selectedDrills: Array,
+    selectedWeights: Array,
+    numberDrills: Number,
+    numberWeights: Number,
+    highlightedPlayer: String,
+    highlightedPosition: Number
+  },
+  computed: {
+    isInselectedDrills() {
+      this.numberDrills; // 因為computed不聽array，所以技巧性重新調動
+      for (let section of this.selectedDrills) {
+        if (JSON.stringify(section) == JSON.stringify([this.day, this.section])) {
+          return true;
+        }
+      }
+      return false;
+    },
+    isInselectedWeights() {
+      this.numberWeights; // 因為computed不聽array，所以技巧性重新調動
+      for (let section of this.selectedWeights) {
+        if (JSON.stringify(section) == JSON.stringify([this.day, this.section])) {
+          return true;
+        }
+      }
+      return false;
+    },
+    isSelectable() {
+      if (this.category == 1) {
+        return (this.numberDrills < 2) ? true : false
+      } else {
+        return (this.numberWeights < 1) ? true : false
+      }
+    },
+    isHighlighted() {
+      return this.highlightedPlayer == this.player;
+    },
+    isPitcher() {
+      return this.highlightedPosition == 1 && this.positions.includes("pitcher");
+    },
+    isCatcher() {
+      return this.highlightedPosition == 2 && this.positions.includes("catcher");
+    },
+    isInfielder() {
+      return this.highlightedPosition == 3 && this.positions.includes("infielder");
+    },
+    isOutfielder() {
+      return this.highlightedPosition == 4 && this.positions.includes("outfielder");
+    }
+  },
+  data: function () {
+    return {
+      days: [...Array(7).keys()],
+      isSelected: false
+    };
+  },
+  methods: {
+    toggle() {
+      if (
+        (this.category == 1 && this.numberDrills < 2) 
+        || (this.category == 2 && this.numberWeights < 1) 
+        || (this.isInselectedDrills || this.isInselectedWeights)) {
+        this.$emit('updateSections', [this.player, this.day, this.section, this.category]);
+      }
+    }
+  },
+  mounted: function() {
+  }
+};
+</script>
+
+<style>
+.player-section-button {
+  display: inline-block;
+  box-sizing: border-box;
+  height: 20px;
+  line-height: 20px;
+  width: 20px;
+  margin: 2px 2px;
+  text-align: center;
+  vertical-align: middle;
+}
+.player-section-button .selectable {
+  border: 1px solid rgb(39, 60, 86);
+}
+.player-section-button .drill {
+  border: 1px solid rgb(39, 60, 86);
+  background-color: rgb(39, 60, 86);
+  color: white;
+}
+.player-section-button .weight {
+  border: 1px solid rgb(206, 49, 49);
+  background-color: rgb(206, 49, 49);
+  color: white;
+}
+.player-section-button .unselectable {
+  border: 1px solid rgb(187, 218, 255);
+  background-color: white;
+  color: rgb(187, 218, 255);
+  cursor: context-menu;
+}
+.player-section-button .highlighted {
+  border: 1px solid rgb(255, 197, 37);
+  background-color: rgb(255, 197, 37);
+  color: white;
+}
+.player-section-button .pitcher {
+  border: 1px solid rgb(255, 75, 144);
+  background-color: rgb(255, 75, 144);
+  color: white;
+}
+.player-section-button .catcher {
+  border: 1px solid rgb(0, 99, 228);
+  background-color: rgb(0, 99, 228);
+  color: white;
+}
+.player-section-button .infielder {
+  border: 1px solid rgb(255, 239, 93);
+  background-color: rgb(255, 239, 93);
+  color: rgb(39, 60, 86);
+}
+.player-section-button .outfielder {
+  border: 1px solid rgb(51, 156, 2);
+  background-color: rgb(51, 156, 2);
+  color: white;
+}
+</style>
